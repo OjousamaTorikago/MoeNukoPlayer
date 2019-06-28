9bb35e13de9487be171a2028f28309a1642a7582
Ojousama <170586@jc-21.jp>
2019-06-28 12:49:33 +0900

    Initialize repository

---
+++ Project/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MoeNukoStudio {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(301, 415);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(101, 12);
+            this.label.TabIndex = 1;
+            this.label.Text = "音を選択してください";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(170, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(394, 280);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.lbx_SelectedIndexChanged);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MoeNukoStudio";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Project/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MoeNukoStudio {
+    public partial class Form1 : Form {
+        string filePathName;
+        private System.Media.SoundPlayer player = null;
+        const string V = @"C:\Users\170586\Desktop\unyahoi\1_人間ちゃん\";
+        string[] files = Directory.GetFiles(V, "*", SearchOption.AllDirectories);
+
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = !KeyPreview;
+            getFileList();
+
+        }
+
+        // 同時再生できるクールなプレイヤー
+        private void WMPlaySound(string wavFile)
+        {
+            WMPLib.WindowsMediaPlayer mediaPlayer = new WMPLib.WindowsMediaPlayer();
+            mediaPlayer.URL = wavFile;
+            mediaPlayer.controls.play();
+        }
+
+        //WAVEファイルを再生する
+        private void PlaySound(string waveFile)
+        {
+            if (player != null) StopSound(); //再生されているときは止める
+            player = new System.Media.SoundPlayer(waveFile); //読み込む
+            player.Play(); //非同期再生する
+
+            //player.PlayLooping(); //次のようにすると、ループ再生される
+            //player.PlaySync();    //次のようにすると、最後まで再生し終えるまで待機する
+        }
+
+        //再生されている音を止める
+        private void StopSound()
+        {
+            if (player != null)
+            {
+                player.Stop();
+                player.Dispose();
+                player = null;
+            }
+        }
+
+        private void fm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Q)
+            {
+
+                WMPlaySound(filePathName);
+            }
+        }
+
+        private void getFileList()
+        {
+
+            string[] fname = new string[files.Count()];
+            int i = 0;
+            foreach(string f in files)
+            {
+                fname[i] = Path.GetFileName(f);
+                i++;
+            }
+            listBox1.Items.AddRange(fname);
+        }
+
+        private void lbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label.Text = listBox1.Text + "をQキーに選択しました。";
+            filePathName = V + listBox1.Text;
+        }
+
+        private void listBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down) return;
+            
+            e.SuppressKeyPress = true;
+            
+        }
+    }
+}
